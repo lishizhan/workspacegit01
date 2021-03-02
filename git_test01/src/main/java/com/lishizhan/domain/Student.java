e5df9dc39774130ad95ed4b4c53d5cd2b6608d42
lishizhan <1575234570@qq.com>
2021-03-02 15:05:40 +0800

    在Student类中添加属性

---
+++ git_test01/src/main/java/com/lishizhan/domain/Student.java
@@ -0,0 +1,11 @@
+package com.lishizhan.domain;
+
+/**
+ * @Author : 黎仕展
+ * @Date : 2021/3/2/0002 15:04
+ * @email : 1575234570@qq.com
+ * @Description :
+ */
+public class Student {
+    private String id;
+}
